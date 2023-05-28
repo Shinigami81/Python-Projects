18e5e524e829ac5c7afb03698d2030fbb3ecf796
Shinigami <122274261+Shinigami81@users.noreply.github.com>
2023-05-28 11:56:15 +0200

    Add files via upload

---
+++ fiz_buzz.py
@@ -0,0 +1,12 @@
+def FizzBuzz():
+    for x in range (1,101):
+        if x % 3 == 0 and x % 5 == 0:
+          print("FizzBuzz")
+        elif x % 5 == 0:
+          print("Buzz")  
+        elif x %3 == 0: 
+          print("Fizz")
+        else:
+          print(x)
+    exit()               
+print(FizzBuzz())
